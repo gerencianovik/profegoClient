e289b379690996acb8fddcdeb853b5b5b13f1a2f
RJEduHuib <rjeduhuib@gmail.com>
2024-08-11 16:45:59 -0500

    cambios

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 const MYSQLHOST = 'localhost';
-const MYSQLUSER = 'root';
-const MYSQLPASSWORD = '';
-const MYSQLDATABASE = 'profego';
+const MYSQLUSER = 'robin';
+const MYSQLPASSWORD = '0987021692';
+const MYSQLDATABASE = 'admin_profeGo';
 const MYSQLPORT = '3306'; // Puerto de la base de datos
 const MYSQL_URI = process.env.MYSQL_URI ?? ''; // URI de conexión a la base de datos (si es necesario)
 
